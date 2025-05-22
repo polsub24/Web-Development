1782e377b2734206b612213d7e0e1cc6f2385b8f
polsub24 <poltu2108@gmail.com>
2025-05-22 17:51:20 +0530

    reactintro

---
+++ Reactjs/element&Style.js
@@ -0,0 +1,11 @@
+const elementStyle = {
+  fontSize: '14px',
+  backgroundColor: '#fafafa',
+};
+
+const element = (
+  <div style={elementStyle} className="myClass">
+    <h1>Hello, World!</h1>
+    <p>Welcome to our application!</p>
+  </div>
+);
